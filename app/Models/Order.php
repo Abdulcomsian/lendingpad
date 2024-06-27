984d0b74763248303c6dcb105c7d0a0e391429e1
abidali05 <abid.ali@accrualhub.com>
2024-06-27 14:34:13 +0500

    utility api
    utility api

---
+++ app/Models/Order.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'loan_number',
+        'myutilities_order_id',
+    ];
+}
