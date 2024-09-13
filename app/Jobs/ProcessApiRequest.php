ec7117f7b452a55cf1fd40eaaec285e3bf282e1a
Abid Ali <abid.ali@accrualhub.com>
2024-09-13 12:27:05 +0500

    true false value validation

--- app/Jobs/ProcessApiRequest.php
+++ app/Jobs/ProcessApiRequest.php
@@ -30,35 +30,146 @@ class ProcessApiRequest implements ShouldQueue
     /**
      * Execute the job.
      */
+    // public function handle()
+    // {
+    //     $json_data = $this->json_data;
+
+    //     Log::info('Received JSON Data: ' . json_encode($json_data));
+
+    //     try {
+    //         // if (!isset($json_data[0]['dates']) || !isset($json_data[0]['dates']['funded']) || is_null($json_data[0]['dates']['funded'])) {
+    //         //     Log::error('Funded Date is Required:');
+    //         //     return;
+    //         // }
+
+    //         // if (!isset($json_data[0]['borrowers']) || count($json_data[0]['borrowers']) == 0) {
+    //         //     Log::error('Borrower information not found:');
+    //         //     return;
+    //         // }
+
+    //         $borrower = $json_data[0]['borrowers'][0];
+
+    //         // if (
+    //         //     !isset($borrower['firstName']) || empty($borrower['firstName']) ||
+    //         //     !isset($borrower['lastName']) || empty($borrower['lastName']) ||
+    //         //     !isset($borrower['contacts']) ||
+    //         //     !isset($borrower['contacts']['email']) || empty($borrower['contacts']['email']) ||
+    //         //     !isset($borrower['contacts']['mobilePhone']) || empty($borrower['contacts']['mobilePhone'])
+    //         // ) {
+    //         //     Log::error('Borrower required fields are missing or null:');
+    //         //     return;
+    //         // }
+
+    //         $data = [
+    //             'name' => $borrower['firstName'] . ' ' . $borrower['lastName'],
+    //             'email' => $borrower['contacts']['email'],
+    //             'phone' => $borrower['contacts']['mobilePhone'],
+    //             'source' => 'affiliate',
+    //             'close_date' => $json_data[0]['dates']['closed'] ?? '',
+    //             'affiliate_order_id' => $json_data[0]['affiliate_order_id'] ?? '',
+    //             'affiliate_order_source' => $json_data[0]['affiliate_order_source'] ?? '',
+    //             'comments' => $json_data[0]['loanOfficer']['name'] ?? '',
+    //             'address_attributes' => [
+    //                 'street1' => $borrower['currentAddress']['street'],
+    //                 'street2' => $borrower['currentAddress']['street2'] ?? '',
+    //                 'city' => $borrower['currentAddress']['city'],
+    //                 'state' => $borrower['currentAddress']['state'],
+    //                 'zipcode' => $borrower['currentAddress']['zipCode'],
+    //             ],
+    //             'loan_number' => $json_data[0]['loanNumber'] ?? '',
+    //         ];
+
+    //         // Check for existing order
+    //         $existingOrder = Order::where('loan_number', $data['loan_number'])->first();
+    //         if ($existingOrder) {
+    //             Log::error('Order already exists for loan number: ' . $data['loan_number']);
+    //             return;
+    //         }
+
+    //         $response = Http::withHeaders([
+    //             'Content-Type' => 'application/json',
+    //             'X-Version' => '1.0',
+    //         ])->post("https://portal.myutilities.com/api/referral/orders?token=4f19bad363ef46de9bf2cbc672a2fba8", [
+    //             'name' => $data['name'],
+    //             'email' => $data['email'],
+    //             'phone' => $data['phone'],
+    //             'source' => $data['source'],
+    //             'close_date' => $data['close_date'],
+    //             'affiliate_order_id' => $data['affiliate_order_id'],
+    //             'affiliate_order_source' => $data['affiliate_order_source'],
+    //             'comments' => $data['comments'],
+    //             'address_attributes' => [
+    //                 'street1' => $data['address_attributes']['street1'],
+    //                 'street2' => $data['address_attributes']['street2'],
+    //                 'city' => $data['address_attributes']['city'],
+    //                 'state' => $data['address_attributes']['state'],
+    //                 'zipcode' => $data['address_attributes']['zipcode'],
+    //             ],
+    //             'loan_number' => $data['loan_number'],
+    //         ]);
+
+    //         if ($response->successful()) {
+    //             Log::info('API call successful: ' . $response->body());
+
+    //             $responseData = json_decode($response->body(), true);
+
+    //             Order::create([
+    //                 'loan_number' => $data['loan_number'],
+    //                 'myutilities_order_id' => $responseData['id'] ?? null,
+    //             ]);
+    //         } else {
+    //             Log::error('API call failed: ' . $response->body());
+    //         }
+    //     } catch (\Exception $e) {
+    //         Log::error('Exception occurred: ' . $e->getMessage());
+    //     }
+    // }
+
     public function handle()
     {
         $json_data = $this->json_data;
 
-        Log::info('Received JSON Data: ' . json_encode($json_data));
+        foreach ($json_data as $item) {
+            if (isset($item['customFields'])) {
+                foreach ($item['customFields'] as $field) {
+                    if (isset($field['name']) && $field['name'] === 'My Utilities Ordered') {
+                        if (isset($field['values']) && is_array($field['values'])) {
+                            if (isset($field['values'][0])) {
+                                $value = $field['values'][0];
 
+                                if ($value === true) {
+                                    Log::info('My Utilities Ordered is true');
+
+                                    $this->sendDataToApi($json_data);
+
+                                    return;
+                                } elseif ($value === false) {
+                                    Log::error('My Utilities Ordered is not true');
+                                    return;
+                                } else {
+                                    Log::error('Unexpected value for My Utilities Ordered');
+                                    return;
+                                }
+                            } else {
+                                Log::error('No values found for My Utilities Ordered');
+                                return;
+                            }
+                        } else {
+                            Log::error('The "values" key is not set or is not an array');
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+
+        Log::error('My Utilities Ordered field not found in customFields');
+    }
+
+    protected function sendDataToApi($json_data)
+    {
         try {
-            if (!isset($json_data[0]['dates']) || !isset($json_data[0]['dates']['funded']) || is_null($json_data[0]['dates']['funded'])) {
-                Log::error('Funded Date is Required:');
-                return;
-            }
-
-            if (!isset($json_data[0]['borrowers']) || count($json_data[0]['borrowers']) == 0) {
-                Log::error('Borrower information not found:');
-                return;
-            }
-
             $borrower = $json_data[0]['borrowers'][0];
-
-            if (
-                !isset($borrower['firstName']) || empty($borrower['firstName']) ||
-                !isset($borrower['lastName']) || empty($borrower['lastName']) ||
-                !isset($borrower['contacts']) ||
-                !isset($borrower['contacts']['email']) || empty($borrower['contacts']['email']) ||
-                !isset($borrower['contacts']['mobilePhone']) || empty($borrower['contacts']['mobilePhone'])
-            ) {
-                Log::error('Borrower required fields are missing or null:');
-                return;
-            }
 
             $data = [
                 'name' => $borrower['firstName'] . ' ' . $borrower['lastName'],
@@ -79,7 +190,6 @@ class ProcessApiRequest implements ShouldQueue
                 'loan_number' => $json_data[0]['loanNumber'] ?? '',
             ];
 
-            // Check for existing order
             $existingOrder = Order::where('loan_number', $data['loan_number'])->first();
             if ($existingOrder) {
                 Log::error('Order already exists for loan number: ' . $data['loan_number']);
